770e56512fdcc362111cf04ebc42de867b39cf73
Gary Brandon <gary.brandon@gmail.com>
2019-11-05 11:39:56 +1100

    Defined VPC stack and set up basic environment configuration

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-
+import os
 from aws_cdk import core
 
 from domains.domain_stack import DomainStack
+from vpcs.vpc_stack import VPCStack
+
+
+default_env = {'account': os.getenv('AWS_ACCOUNT', os.getenv('CDK_DEFAULT_ACCOUNT', '')),
+               'region': os.getenv('AWS_DEFAULT_REGION', os.getenv('CDK_DEFAULT_REGION', ''))}
 
 
 app = core.App()
-DomainStack(app, "DomainStack")
+DomainStack(app, "DomainStack", env=default_env)
+VPCStack(app, "VPCStack", env=default_env)
 
 app.synth()
---
+++ vpcs/vpc_stack.py
@@ -0,0 +1,16 @@
+from aws_cdk import core
+from aws_cdk.aws_ec2 import Vpc, SubnetType, SubnetConfiguration
+
+
+class VPCStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+       
+        subnet_conf = [SubnetConfiguration(cidr_mask=24, name='public', subnet_type=SubnetType.PUBLIC)]
+
+        vpc = Vpc(self, 'QuakeServicesVPC', 
+                cidr='172.16.0.0/16',
+                nat_gateways=0,
+                subnet_configuration=subnet_conf)
+
