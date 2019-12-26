e520cde43ffdaf02a6311bc2627d2ebb629ecadf
gazwald <gary.brandon@gmail.com>
2019-12-26 13:21:18 +1100

    Removed need for NAT Gateways with VPC Endpoints

--- vpcs/vpc_stack.py
+++ vpcs/vpc_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-from aws_cdk.aws_ec2 import Vpc, SubnetType, SubnetConfiguration
+from aws_cdk.aws_ec2 import Vpc, SubnetType, SubnetConfiguration, GatewayVpcEndpointAwsService
 
 
 class VPCStack(core.Stack):
@@ -7,12 +7,26 @@ class VPCStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
        
-        subnet_conf = list()
-        subnet_conf.append(SubnetConfiguration(cidr_mask=24, name='public', subnet_type=SubnetType.PUBLIC))
-        subnet_conf.append(SubnetConfiguration(cidr_mask=24, name='private', subnet_type=SubnetType.PRIVATE))
+        subnet_public = SubnetConfiguration(cidr_mask=24, name='public', subnet_type=SubnetType.PUBLIC)
+        subnet_private = SubnetConfiguration(cidr_mask=24, name='private', subnet_type=SubnetType.ISOLATED)
+        subnet_conf = [subnet_public, subnet_private]
 
         self.vpc = Vpc(self, 'QuakeServicesVPC', 
                        cidr='172.16.0.0/16',
-                       nat_gateways=1,
+                       nat_gateways=0,
                        subnet_configuration=subnet_conf)
+
+        self.vpc.add_s3_endpoint('s3-endpoint', 
+                                 subnets=[subnet_private])
+
+        self.vpc.add_dynamo_db_endpoint('dynamo-endpoint',
+                                        subnets=[subnet_private])
+
+        self.vpc.add_gateway_endpoint('ecr-endpoint',
+                                      service=GatewayVpcEndpointAwsService('ecr-service', prefix='ecr'),
+                                      subnets=[subnet_private])
+
+        self.vpc.add_gateway_endpoint('ecs-endpoint',
+                                      service=GatewayVpcEndpointAwsService('ecs-service', prefix='ecs'),
+                                      subnets=[subnet_private])
 
