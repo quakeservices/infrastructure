5c14cf27467e4cb04d105d70b9fce87bfe682a35
gazwald <gary.brandon@gmail.com>
2020-01-01 13:10:01 +1100

    Added DNS validated certificate in us-east-1 and us-west-2 to domain module

--- domains/domain_stack.py
+++ domains/domain_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import core
 import aws_cdk.aws_route53 as route53
+import aws_cdk.aws_certificatemanager as certificatemanager
 
 
 class DomainStack(core.Stack):
@@ -7,9 +8,25 @@ class DomainStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        domains = ['quake.services',
-                   'quake2.services',
-                   'quake3.services']
+        regions = ['us-west-2', 'us-east-1']
 
+        primary_domain = 'quake.services'
+        secondary_domains = ['quake2.services',
+                             'quake3.services']
+
+        domains = [primary_domain] + secondary_domains
+
+        subject_alt = secondary_domains + ['*.' + x for x in domains]
+        print(subject_alt)
+
+        zones = dict()
         for domain in domains:
-            route53.PublicHostedZone(self, domain, zone_name=domain)
+            zones[domain] = route53.PublicHostedZone(self, domain, zone_name=domain)
+
+        for region in regions:
+            certificate = certificatemanager.DnsValidatedCertificate(self, 'wildcard_' + region,
+                    hosted_zone=zones[primary_domain],
+                    region=region,
+                    domain_name=primary_domain,
+                    subject_alternative_names=subject_alt,
+                    validation_method=certificatemanager.ValidationMethod.DNS)
