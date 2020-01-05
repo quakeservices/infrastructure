218846d2dcbc99516d5e8c4c2a273b8a205237ed
gazwald <gary.brandon@gmail.com>
2020-01-05 12:47:09 +1100

    Adding a NAT Gateway to make things easier initially

--- vpcs/vpc_stack.py
+++ vpcs/vpc_stack.py
@@ -8,12 +8,13 @@ class VPCStack(core.Stack):
         super().__init__(scope, id, **kwargs)
        
         subnet_public = SubnetConfiguration(cidr_mask=24, name='public', subnet_type=SubnetType.PUBLIC)
-        subnet_private = SubnetConfiguration(cidr_mask=24, name='private', subnet_type=SubnetType.ISOLATED)
+        subnet_private = SubnetConfiguration(cidr_mask=24, name='private', subnet_type=SubnetType.PRIVATE)
         subnet_conf = [subnet_public, subnet_private]
 
         self.vpc = Vpc(self, 'QuakeServicesVPC', 
                        cidr='172.16.0.0/16',
-                       nat_gateways=0,
+                       nat_gateways=1,
+                       max_azs=3,
                        subnet_configuration=subnet_conf)
 
         self.vpc.add_s3_endpoint('s3-endpoint', 
