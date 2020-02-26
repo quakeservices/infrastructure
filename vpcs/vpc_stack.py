706266b4310d7edeb53f19d451d4f312591cd539
gazwald <gary.brandon@gmail.com>
2020-02-26 12:14:04 +1100

    Cleaning up imports

--- vpcs/vpc_stack.py
+++ vpcs/vpc_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-from aws_cdk.aws_ec2 import Vpc, SubnetType, SubnetConfiguration, GatewayVpcEndpointAwsService
+import aws_cdk.aws_ec2 as ec2
 
 
 class VPCStack(core.Stack):
@@ -7,23 +7,23 @@ class VPCStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
        
-        subnet_public = SubnetConfiguration(cidr_mask=24, name='public', subnet_type=SubnetType.PUBLIC)
-        subnet_private = SubnetConfiguration(cidr_mask=24, name='private', subnet_type=SubnetType.PRIVATE)
+        subnet_public = ec2.SubnetConfiguration(cidr_mask=24, name='public', subnet_type=ec2.SubnetType.PUBLIC)
+        subnet_private = ec2.SubnetConfiguration(cidr_mask=24, name='private', subnet_type=ec2.SubnetType.PRIVATE)
         subnet_conf = [subnet_public, subnet_private]
 
-        self.vpc = Vpc(self, 'QuakeServicesVPC', 
-                       cidr='172.16.0.0/16',
-                       nat_gateways=1,
-                       max_azs=3,
-                       subnet_configuration=subnet_conf)
+        self.vpc = ec2.Vpc(self, 'QuakeServicesVPC',
+                           cidr='172.16.0.0/16',
+                           nat_gateways=1,
+                           max_azs=3,
+                           subnet_configuration=subnet_conf)
 
         self.vpc.add_gateway_endpoint(
                 's3-endpoint',
-                service=GatewayVpcEndpointAwsService('s3'))
+                service=ec2.GatewayVpcEndpointAwsService('s3'))
 
         self.vpc.add_gateway_endpoint(
                 'dynamo-endpoint',
-                service=GatewayVpcEndpointAwsService('dynamodb'))
+                service=ec2.GatewayVpcEndpointAwsService('dynamodb'))
 
         self.vpc.add_flow_log("FlowLog")
 
